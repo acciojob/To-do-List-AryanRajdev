afd8fd88bd6a3755fd720fb521477527128ce008
AryanRajdev <91838247+AryanRajdev@users.noreply.github.com>
2025-03-27 22:07:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here
+let ol = document.getElementById("todoList");
+document.getElementById("addTodoBtn").addEventListener("click",()=>{
+	let input = document.getElementById("newTodoInput").value;
+	let li = document.createElement("li");
+	if(input!==""){
+		li.textContent = input;
+		ol.appendChild(li);
+	}
+	
+});
 
+document.getElementById("newTodoInput").addEventListener("click",()=>{
+	document.getElementById("newTodoInput").value = "";
+});
